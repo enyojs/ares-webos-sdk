933d732b4c7a0a02687741505eace1578ace0912
Yves Del-Medico <yves.del-medico@hp.com>
2013-02-14 19:32:58 +0100

    ENYO-1783: Integrated ares-generate.sh and ares-package.sh  - Moved palm-*.js into lib/ares-*.js  - Updated ares-*.sh to use .js files under lib
    Enyo-DCO-1.1-Signed-off-by: Yves Del Medico <yves.del-medico@hp.com>

---
+++ ares-package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# the folder this script is in
+ARES_DIR=$(cd `dirname $0` && pwd)
+
+# node script we are going to run
+PACKAGE="$ARES_DIR/lib/ares-package.js"
+
+# path to included node
+export NODE_PATH="$ARES_DIR/node/bin"
+
+# run node script with imported params
+PATH=$PATH:$NODE_PATH node $PACKAGE $@
