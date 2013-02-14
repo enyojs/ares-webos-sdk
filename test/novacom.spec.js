177da8778b26dd7c5e971c0a581c1872b00d1067
Francois-Xavier KOWALSKI <francois-xavier.kowalski@hp.com>
2013-02-14 17:34:48 +0100

    ENYO-1967: implement `novacom run` over SSH + more tests
    - fix race conditions using temporary files, but using `Buffer`-based
      streams instead (add a dependency on `stream-buffers`)
    - so far there is a small global namespace pollution bug in
      `stream-buffers`.  Issue published (with patch) at:
      https://github.com/samcday/node-stream-buffer/issues/4
    - add 2 tests for `novacom run`
    
    Enyo-DCO-1.1-Signed-off-by: Francois-Xavier KOWALSKI <francois-xavier.kowalski@hp.com>

--- test/novacom.spec.js
+++ test/novacom.spec.js
@@ -3,10 +3,10 @@
 var path = require('path'),
     fs = require('fs'),
     util = require('util'),
-    temp = require("temp"),
     log = require('npmlog'),
     nopt = require('nopt'),
     should = require('should'),
+    streamBuffers = require("stream-buffers"),
     novacom = require(path.join(__dirname, '..', 'lib', 'novacom'));
 
 var knownOpts = {
@@ -61,25 +61,24 @@ function cleanTmp(done) {
 }
 
 function mkReadableStream(data) {
-	var buf;
-	if (Buffer.isBuffer(data)) {
-		buf = data;
-	} else {
-		buf = new Buffer(data);
-	}
-	var tmp = temp.path({prefix: 'mocha-novacom.'});
-	log.verbose("mkReadableStream", "creating " + tmp);
-	var os = fs.createWriteStream(tmp);
-	os.write(buf);
-	os.end();
-	tmps.push(tmp);
-	var is = fs.createReadStream(tmp);
+	var is = new streamBuffers.ReadableStreamBuffer({
+		frequency: 10,       // in milliseconds.
+		chunkSize: 2048     // in bytes.
+	});
 	is.pause();
+	is.put(data);
 	return is;
 }
 
+function mkWritableStream() {
+	return new streamBuffers.WritableStreamBuffer({
+		initialSize: (100 * 1024),      // start as 100 kilobytes.
+		incrementAmount: (10 * 1024)    // grow by 10 kilobytes each time buffer overflows.
+	});
+}
+
 describe("novacom", function() {
-	this.timeout(5000);
+	this.timeout(7000);
 
 	var sampleText = "This is a sample text.";
 
@@ -108,7 +107,7 @@ describe("novacom", function() {
 			session.put(deviceTmp, is, function(err) {
 				is.destroy();
 				should.exist(err);
-				err.should.be.an.instanceof(Error);
+				err.should.be.an.instanceOf(Error);
 				should.exist(err.code);
 				err.code.should.equal(1);
 				done();
@@ -131,26 +130,60 @@ describe("novacom", function() {
 				should.not.exist(err);
 				is.destroy();
 				
-				var hostTmp = temp.path({prefix: 'mocha-novacom.'});
-				var os = fs.createWriteStream(hostTmp);
+				var os = mkWritableStream();
 				log.verbose("get()", "...");
 				session.get(deviceTmp, os, function(err) {
 					log.verbose("get()", "done");
 					should.not.exist(err);
 					os.end();
-					log.verbose("readFile()", "...");
-					fs.readFile(hostTmp, function(err, buf) {
-						log.verbose("readFile()", "done");
-						should.not.exist(err);
-						should.exist(buf);
-						buf.should.be.an.instanceof(Buffer);
-						var str = buf.toString();
-						str.should.equal(sampleText);
-						done();
-					});
+					var str = os.getContents().toString();
+					str.should.equal(sampleText);
+					done();
 				});
 			});
 		});
 	});
 
+	describe("#run", function() {
+		beforeEach(initTmp);
+		afterEach(cleanTmp);
+
+		beforeEach(openSession);
+		afterEach(closeSession);
+
+		it("should write a file on the device and 'ls' it successfully", function(done) {
+			var is = mkReadableStream(sampleText);
+			log.verbose("put()", "...");
+			session.put(deviceTmp, is, function(err) {
+				log.verbose("put()", "done");
+				should.not.exist(err);
+				is.destroy();
+
+				var os = mkWritableStream();
+				var es = mkWritableStream();
+				log.verbose("run()", "...");
+				session.run('/bin/ls -l ' + deviceTmp, null /*stdin*/, os /*stdout*/, es /*stderr*/, function(err) {
+					log.verbose("run()", "done");
+					should.not.exist(err);
+					os.end();
+					var str = os.getContents().toString();
+					var length = str.split(/[ \t]+/)[4];
+					should.exist(length);
+					length.should.equal(sampleText.length.toString(), "length of '" + sampleText + "'");
+					done();
+				});
+			});
+		});
+
+		it("should fail to 'ls' a non-existing file", function(done) {
+			log.verbose("run()", "...");
+			var os = mkWritableStream();
+			var es = mkWritableStream();
+			session.run('/bin/ls -l /dev/null/toto', null /*stdin*/, os /*stdout*/, es /*stderr*/, function(err) {
+				log.verbose("run()", "done err=" + err);
+				should.exist(err);
+				done();
+			});
+		});
+	});
 });
