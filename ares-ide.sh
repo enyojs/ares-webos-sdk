8db78109d351046a3af84f4e651a66327a99d5a7
Andrew Rich <andrew.rich@palm.com>
2013-02-14 21:58:20 -0800

    wrapper script for ares IDE

---
+++ ares-ide.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# the folder this script is in
+ARES_DIR=$(cd `dirname $0` && pwd)
+
+# path to included node
+export NODE_PATH="$ARES_DIR/node/bin"
+
+# launch ares IDE
+cd ares-project
+PATH=$NODE_PATH:$PATH node ide.js
