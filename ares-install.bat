6d1d39c506f2befee01ddb626751e29fe977a474
Yves Del-Medico <yves.del-medico@hp.com>
2013-02-25 13:54:14 +0100

    ENYO-2006: Added ares-install and ares-launch wrappers for Windows
    Enyo-DCO-1.1-Signed-off-by: Yves Del Medico <yves.del-medico@hp.com>

---
+++ ares-install.bat
@@ -0,0 +1,14 @@
+@REM don't watch the sausage being made
+@ECHO OFF
+
+REM the folder this script is in (*/bootplate/tools)
+SET ARES_DIR=%~DP0
+
+REM node script we are going to run
+SET JS_SCRIPT=%ARES_DIR%\lib\ares-install.js
+
+REM node location
+SET NODE=node.exe
+
+REM use node to invoke js script with imported parameters
+%NODE% "%JS_SCRIPT%" %*
