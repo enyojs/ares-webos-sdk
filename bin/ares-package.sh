c427054c814e7b6d26366138862d4eb154e828de
Francois-Xavier KOWALSKI <francois-xavier.kowalski@hp.com>
2013-05-06 00:06:22 -0700

    ENYO-2339: rename `webos-sdk` into `ares-webos-sdk`
    Enyo-DCO-1.1-Signed-off-by: Francois-Xavier KOWALSKI <francois-xavier.kowalski@hp.com>

--- bin/ares-package.sh
+++ bin/ares-package.sh
@@ -4,7 +4,7 @@
 BIN_DIR=$(cd `dirname $0` && pwd)
 
 # node script we are going to run
-SCRIPT=${BIN_DIR}/../lib/node_modules/webos-sdk/bin/ares-package.js
+SCRIPT=${BIN_DIR}/../lib/node_modules/ares-webos-sdk/bin/ares-package.js
 
 # path to node modules
 export NODE_PATH=$(cd ${BIN_DIR}/../lib && pwd)
