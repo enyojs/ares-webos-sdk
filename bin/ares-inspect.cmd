6af0c154732657439611d258156ada6ab95725dd
Junil Kim <junilkim@Junil-ui-MacBook-Pro.local>
2013-07-18 17:27:18 +0900

    add script file to launch ares-inspect node module
    Enyo-DCO-1.1-Signed-off-by: Junil Kim <logyourself@gmail.com>

---
+++ bin/ares-inspect.cmd
@@ -0,0 +1,6 @@
+:: Created by npm, please don't edit manually.
+@IF EXIST "%~dp0\node.exe" (
+  "%~dp0\node.exe"  "%~dp0\.\node_modules\ares-webos-sdk\bin\ares-inspect.js" %*
+) ELSE (
+  node  "%~dp0\.\node_modules\ares-webos-sdk\bin\ares-inspect.js" %*
+)
