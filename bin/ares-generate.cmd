71b3ed317aafe81148ed67157272ea502096e645
Junil Kim <logyourself@gmail.com>
2013-07-19 02:55:08 +0900

    ENYO-2677:script under bin directory can find the node module properly
    Enyo-DCO-1.1-Signed-off-by: Junil Kim <logyourself@gmail.com>

--- bin/ares-generate.cmd
+++ bin/ares-generate.cmd
@@ -1,6 +1,12 @@
 :: Created by npm, please don't edit manually.
+@SET SCRIPT="%~dp0\.\node_modules\ares-webos-sdk\bin\ares-generate.js"
+
+@IF NOT EXIST %SCRIPT% (
+    @SET SCRIPT="%~dp0\.\ares-generate.js"
+) 
+
 @IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\.\node_modules\ares-webos-sdk\bin\ares-generate.js" %*
+  "%~dp0\node.exe"  %SCRIPT% %*
 ) ELSE (
-  node  "%~dp0\.\node_modules\ares-webos-sdk\bin\ares-generate.js" %*
+  node  %SCRIPT% %*
 )
