83087cc5f461ed81ade2fd6fbc21de434ab67d50
Junil Kim <junilkim@Junil-ui-MacBook-Pro.local>
2013-07-18 17:46:06 +0900

    ENYO-2677: fixed script to find path of ares-inspect.js
    Enyo-DCO-1.1-Signed-off-by: Junil Kim <logyourself@gmail.com>

--- bin/ares-inspect.sh
+++ bin/ares-inspect.sh
@@ -6,6 +6,14 @@ BIN_DIR=$(cd `dirname $0` && pwd)
 # node script we are going to run
 SCRIPT=${BIN_DIR}/../lib/node_modules/ares-webos-sdk/bin/ares-inspect.js
 
+# check file path
+if [ ! -e $SCRIPT ]; then
+    echo "no file file found"
+    SCRIPT=${BIN_DIR}/ares-inspect.js
+else
+    echo "file exist"
+fi
+
 # path to node modules
 export NODE_PATH=$(cd ${BIN_DIR}/../lib && pwd)
 
