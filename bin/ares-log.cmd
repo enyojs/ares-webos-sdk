fc7b9c518492273c348cd40742e6cae956d018cd
Junil Kim <junilkim@Junil-ui-MacBook-Pro.local>
2014-03-14 16:36:31 +0900

    ENYO-3880: added ares-log to display native application logs from a webOS device
    Enyo-DCO-1.1-Signed-off-by Junil Kim <logyourself@gmail.com>

---
+++ bin/ares-log.cmd
@@ -0,0 +1,12 @@
+:: Created by npm, please don't edit manually.
+@SET SCRIPT="%~dp0\.\node_modules\ares-webos-sdk\bin\ares-log.js"
+
+@IF NOT EXIST %SCRIPT% (
+    @SET SCRIPT="%~dp0\.\ares-log.js"
+) 
+
+@IF EXIST "%~dp0\x86\node.exe" (
+  "%~dp0\x86\node.exe"  %SCRIPT% %*
+) ELSE (
+  node  %SCRIPT% %*
+)
---
+++ bin/ares-log.js
@@ -0,0 +1,265 @@
+#!/usr/bin/env node
+
+var fs  	= require('fs'),
+    path 	= require("path"),
+    npmlog 	= require('npmlog'),
+    nopt 	= require('nopt'),
+    async 	= require('async'),
+    sprintf = require('sprintf').sprintf,
+	ipkg		= require('./../lib/ipkg-tools'),
+    versionTool = require('./../lib/version-tools'),
+    console 	= require('./../lib/consoleSync'),
+    novacom 	= require('./../lib/novacom'),
+    help 		= require('./../lib/helpFormat'),
+	util 		= require('util');
+    
+/**********************************************************************/
+
+var processName = path.basename(process.argv[1]).replace(/.js/, '');
+
+process.on('uncaughtException', function (err) {
+	log.info('exit', err);
+	log.error('exit', err.toString());
+	process.exit(1);
+});
+
+if (process.argv.length === 2) {
+	process.argv.splice(2, 0, '--help');
+}
+
+/**********************************************************************/
+
+var processName = path.basename(process.argv[1]).replace(/.js/, '');
+
+var knownOpts = {
+	//generic options
+	"help":		Boolean,
+	"level":	['silly', 'verbose', 'info', 'http', 'warn', 'error'],
+	"version":	Boolean,
+	// command-specific options
+	"device-list":		Boolean,
+	"list":		Boolean,
+	"port":		[String, Array],
+	"device":	[String, null],
+	// no shortHands
+	"run":		[String, null],
+	"follow":	Boolean,
+};
+
+var shortHands = {
+	// generic aliases
+	"h": ["--help"],
+	"v": ["--level", "verbose"],
+	"V": ["--version"],
+	// command-specific aliases
+	"D": ["--device-list"],
+	"l": ["--list"],
+	"p": ["--port"],
+	"f": ["--follow"],
+	"d": ["--device"]
+};
+
+var helpString = [
+	"",
+	"NAME",
+	help.format(processName + " - Display application logs from a webOS device."),
+	"",
+	"SYNOPSIS",
+	help.format(processName + " [OPTION...] [APP_ID]"),
+//	"Options (Not implmeneted) :",
+//	help.format(processName + " [OPTIONS] --put file://DEVICE_PATH < HOST_FILE"),
+//	help.format(processName + " [OPTIONS] --get file://DEVICE_PATH > HOST_FILE"),
+//	"",
+	"",
+	"OPTION",
+	help.format("-d, --device <DEVICE>", "Specify DEVICE to use"),
+	help.format("-D, --device-list", "List the available DEVICEs"),
+	help.format("-f, --follow", "Follow the log output (use Ctrl-C to terminate)"),
+	help.format("-l, --list", "List the installed app IDs"),
+	help.format("--level <LEVEL>", "Tracing LEVEL is one of 'silly', 'verbose', 'info', 'http', 'warn', 'error' [warn]"),
+	help.format("-h, --help", "Display this help"),
+	help.format("-V, --version", "Display version info"),
+	"",
+	"DESCRIPTION",
+	"",
+	help.format("**Restriction**"),
+	help.format("this command can display only native application logs, not web application."),
+	"", 
+	help.format("APP_ID is sthe id of the application for which logs are shown."),
+	"", 
+	"Examples:",
+	"",
+	"# Display logs for app",
+	processName + " com.yourdomain.app -d emulator",
+	"",
+	"# Follow logs for app",
+	processName + " -f com.yourdomain.app -d emulator",
+	"",
+];
+
+var argv = nopt(knownOpts, shortHands, process.argv, 2 /*drop 'node' & 'ares-*.js'*/);
+
+/**********************************************************************/
+
+var log = npmlog;
+log.heading = processName;
+log.level = argv.level || 'warn';
+
+/**********************************************************************/
+log.verbose("argv", argv);
+argv.appId = (argv.argv.remain.length > 0)? argv.argv.remain[0] : null;
+
+var op;
+if (argv.list) {
+	op = list;
+} else if (argv.device-list) {
+	op = deviceList;
+} else if (argv.run) {
+	op = run;
+} else if (argv.appId) {
+	op = printLog;
+} else if (argv.version) {
+	versionTool.showVersionAndExit();
+} else if (argv.help) {
+	helpString.forEach(function(line) {
+		console.log(line);
+	});
+	process.exit(0);
+} else {
+	process.exit(1);
+}
+
+var options = {
+	device: argv.device
+};
+
+if (op) {
+	versionTool.checkNodeVersion(function(err) {
+		op(finish);
+	});
+}
+
+/**********************************************************************/
+
+function deviceList(next) {
+	var resolver = new novacom.Resolver();
+	async.waterfall([
+		resolver.load.bind(resolver),
+		resolver.list.bind(resolver),
+		function(devices, next) {
+			log.info("list()", "devices:", devices);
+			if (Array.isArray(devices)) {
+				console.log(sprintf("%-16s %-16s %-16s %-16s %s", 
+						"<DEVICE NAME>", "<PLATFORM>", "<PRIVATE KEY>", "<PASSPHRASE>", "<SSH ADDRESS>"));
+				devices.forEach(function(device) {
+					var sshPrvKeyName = device.privateKeyName || "'No Ssh Key'";
+					var sshPassphrase = device.passphrase || "'No passphrase'"
+					console.log(sprintf("%-16s %-16s %-16s %-16s (%s)", 
+						device.name, device.type, sshPrvKeyName, sshPassphrase, device.addr));
+				});
+			}
+			log.info("list()", "Success");
+			next();
+		}
+	], next);
+}
+
+function list(next) {
+	ipkg.installer.list(options, function(err, pkgs) {
+		var strPkgs = "";
+		if (pkgs instanceof Array) pkgs.forEach(function (pkg) {
+			strPkgs = strPkgs.concat(pkg.id).concat('\n');
+		});
+		process.stdout.write(strPkgs);
+		finish(err);
+	});
+}
+
+function isInstalled(appId, next) {
+	var installed = false;
+	ipkg.installer.list(options, function(err, pkgs) {
+		if (pkgs instanceof Array) pkgs.forEach(function (pkg) {
+			if(pkg.id == appId) {
+				installed = true;
+				return;
+			}
+		});
+		if (!installed) {
+			next(new Error(appId + " is not installed."));
+		} else {
+			next();
+		}
+	});
+}
+
+function run(next) {
+	var session = new novacom.Session(options.device, function(err, result) {
+		log.verbose("run()", "argv:", argv.run);
+		log.verbose("run()", "options:", options);
+		if (err) {
+			next(err);
+			return;
+		}
+		session.run(argv.run, process.stdin, process.stdout, process.stderr, next);
+	});
+}
+
+function printLog(next) {
+	log.verbose("printLog()", "options:", options);
+	if (!argv.appId) {
+		return next(new Error("app ID is required to print log"));
+	}
+	if (argv.follow) {
+		argv.follow = "-f";
+	} else {
+		argv.follow = "";
+	}
+	var command = util.format("%s %s %s", "tail", argv.follow, "/media/developer/log/devlog");
+	var session;
+	async.series([
+		isInstalled.bind(null, argv.appId),
+		function(next) {
+			session = new novacom.Session(options.device, next);
+		},
+		function(next) {
+			session.run(command, process.stdin, _onData, process.stderr, next);
+			function _onData(data) {
+				var str;
+				if (Buffer.isBuffer(data)) {
+					str = data.toString();
+				} else {
+					str = data;
+				}
+				str.split(/\r?\n/).forEach(_onLine);
+			}
+			function _onLine(line) {
+				var regExp = new RegExp(argv.appId, "gi");
+				if (line.match(regExp)) {
+					console.log(line);
+				} 
+			}
+		}
+	], function(err, result) {
+		next(err);
+	});
+}
+
+/**********************************************************************/
+
+function finish(err, value) {
+	log.info("finish():", "err:", err);
+	if (err) {
+		console.log(processName + ": "+ err.toString());
+		process.exit(1);
+	} else {
+		log.info('finish():', value);
+		if (value && value.msg) {
+			console.log(value.msg);
+		}
+		process.exit(0);
+	}
+}
+
+process.on('uncaughtException', function (err) {
+	console.log('Caught exception: ' + err);
+});
