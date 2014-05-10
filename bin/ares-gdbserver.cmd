61971cd4690f92a4c23437246d9d99e299035050
Junil Kim <logyourself@gmail.com>
2014-05-10 18:11:04 +0900

    ENYO-4020: single quote problem on windows
    use powershell to get arguments enclosed with signle quotes
    
    if no powershell on windows, convert arguments (workaround)
    
    Enyo-DCO-1.1-Signed-off-by Junil Kim <logyourself@gmail.com>

--- bin/ares-gdbserver.cmd
+++ bin/ares-gdbserver.cmd
@@ -1,15 +1,40 @@
 :: Created by npm, please don't edit manually.
+@ECHO OFF
 @SET SCRIPT="%~dp0\.\node_modules\ares-webos-sdk\bin\ares-gdbserver.js"
 
 @IF NOT EXIST %SCRIPT% (
     @SET SCRIPT="%~dp0\.\ares-gdbserver.js"
 ) 
+@SET args=%*
+
+:: Convert if param has "'{" & "}'" 
+:: Temp way to get JSON param enclosed with single quotes.
+@SET CONVERT=FALSE
+@echo.%* | findstr /C:"'{">nul && (
+           @echo.%* | findstr /C:"}'">nul && (
+                     @SET CONVERT=TRUE
+           )
+) || (
+           @SET CONVERT=FALSE
+)
+
+:: If powershell exist, no need converting
+powershell /? >null 2>&1 && (
+    @SET RUNCMD=cmd /c powershell node
+    @SET CONVERT=FALSE
+) || (
+  @SET RUNCMD=node
+)
+
+@IF %CONVERT%==TRUE (
+    @SET temp1=%args:"=""%
+    @SET args=%temp1:'="%
+)
 
 @IF EXIST "%~dp0\x86\node.exe" (
     @SETLOCAL
     @SET "PATH=%~dp0\x86;%PATH%"
-    node %SCRIPT% %*
+    %RUNCMD% %SCRIPT% %args%
 ) ELSE (
-    node %SCRIPT% %*
+    %RUNCMD% %SCRIPT% %args%
 )
-
--- bin/ares-novacom.cmd
+++ bin/ares-novacom.cmd
@@ -1,14 +1,41 @@
 :: Created by npm, please don't edit manually.
+@ECHO OFF
 @SET SCRIPT="%~dp0\.\node_modules\ares-webos-sdk\bin\ares-novacom.js"
 
 @IF NOT EXIST %SCRIPT% (
     @SET SCRIPT="%~dp0\.\ares-novacom.js"
 ) 
 
+@SET args=%*
+
+:: Convert if param has "'{" & "}'" 
+:: Temp way to get JSON param enclosed with single quotes.
+@SET CONVERT=FALSE
+@echo.%* | findstr /C:"'{">nul && (
+           @echo.%* | findstr /C:"}'">nul && (
+                     @SET CONVERT=TRUE
+           )
+) || (
+           @SET CONVERT=FALSE
+)
+
+:: If powershell exist, no need converting
+powershell /? >null 2>&1 && (
+    @SET RUNCMD=cmd /c powershell node
+    @SET CONVERT=FALSE
+) || (
+  @SET RUNCMD=node
+)
+
+@IF %CONVERT%==TRUE (
+    @SET temp1=%args:"=""%
+    @SET args=%temp1:'="%
+)
+
 @IF EXIST "%~dp0\x86\node.exe" (
     @SETLOCAL
     @SET "PATH=%~dp0\x86;%PATH%"
-    node %SCRIPT% %*
+    %RUNCMD% %SCRIPT% %args%
 ) ELSE (
-  node  %SCRIPT% %*
+    %RUNCMD% %SCRIPT% %args%
 )
